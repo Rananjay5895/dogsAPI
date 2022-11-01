2dc4031690a5bbcc58ed1936156b5b7b512cd1f3
Rananjay5895 <Rananjay5895>
2022-11-01 13:18:08 +0000

    Initial commit

---
+++ src/test/java/com/personal/dogs/DogsAPITest.java
@@ -0,0 +1,21 @@
+package com.personal.dogs;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class DogsAPITest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
